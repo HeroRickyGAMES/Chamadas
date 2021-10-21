c84fca6f50f19232670def67cd15067ba542598c
HeroRickyGAMES <ricojn9@gmail.com>
2021-10-21 11:35:23 -0700

    Uma nova tela principal para adicionar futuramente um modo de calcular as notas, fazendo uma agenda do professor bem mais automatica! Estou feliz por estar desenvolvendo esse aplicativo. XD

---
+++ app/src/main/java/com/herorickystudios/chamadas/PrimeiraTela.java
@@ -0,0 +1,24 @@
+package com.herorickystudios.chamadas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrimeiraTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira_tela);
+    }
+
+    //Botão para a tela de chamadas
+    public void BtnACChamadas(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
